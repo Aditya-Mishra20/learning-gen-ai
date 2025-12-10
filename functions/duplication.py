8e16c5dac61fea7500d135c70028913aae7306c7
Aditya Mishra <contactme@gmail.com>
2025-12-10 17:18:44 +0530

    Add functions for user registration and input validation; implement bill calculation and season-month pairing

---
+++ functions/duplication.py
@@ -0,0 +1,5 @@
+def print_info(name, age):
+    print(f"the name is {name} and the age is {age}")
+    
+    
+print_info("Alice", 30)
---
+++ functions/readability.py
@@ -0,0 +1,8 @@
+def calulate_bill(no_of_cups, price_per_cup):
+    return no_of_cups * price_per_cup
+
+
+res = calulate_bill(3, 20)
+
+
+print(res)
