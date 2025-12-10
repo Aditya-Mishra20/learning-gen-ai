8a2578922d94ec6dab01d4eceec12ac5f268e0b2
Aditya Mishra <contactme@gmail.com>
2025-12-10 17:43:11 +0530

    Add Chai class implementation with flavor attribute and type checks

---
+++ oops/namespace.py
@@ -0,0 +1,17 @@
+class Chai:
+    origin = "India"
+
+
+masala = Chai()
+masala.flavor = "Spicy"
+
+print(masala.flavor)
+
+
+
+
+
+
+
+
+
---
+++ oops/simple_class.py
@@ -0,0 +1,17 @@
+class Chai:
+    pass
+
+
+print(type(Chai))
+
+
+
+ginger_tea = Chai()
+print(ginger_tea)
+print(type(ginger_tea))
+print(type(ginger_tea) is Chai)
+
+
+
+
+
