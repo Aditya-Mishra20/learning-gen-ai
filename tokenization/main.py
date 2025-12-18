d4a04c27e20e6ba5572588d24ca6a8af058875df
Aditya Mishra <contactme@gmail.com>
2025-12-18 23:44:10 +0530

    learning genai- tokenization, encoding and decoding

---
+++ tokenization/main.py
@@ -0,0 +1,16 @@
+# pip freeze > requirements.txt is command that create a file requirements.txt which shows what packages are installed
+# AIzaSyBglCCIlDbNEpf_BeJMqbilxCBo_BG91aU
+import tiktoken
+
+encoder = tiktoken.encoding_for_model("gpt-4o")
+
+text = "Hey there! My name is Aditya"
+print(text)
+tokens = encoder.encode(text)
+
+print(tokens)
+print(len(tokens))
+
+detoken = encoder.decode(tokens)
+
+print(detoken)
