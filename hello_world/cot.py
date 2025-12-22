21164f0b0b5743d726e8cd7d282cccc2a854e848
Aditya Mishra <adityamishra21am@gmail.com>
2025-12-22 21:47:53 +0530

    types of prompting

---
+++ hello_world/cot.py
@@ -0,0 +1,54 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Chain of thaught prompting 
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+client = OpenAI(
+    api_key = gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+SYSTEM_PROMPT = """
+
+You're an expert AI Assistant in resolving user queries using chain of thought.
+You work on START, PLAN and OUPUT steps.
+You need to first PLAN what needs to be done. The PLAN can be multiple steps.
+Once you think enough PLAN has been done, finally you can give an OUTPUT.
+
+Rules:
+
+- Strictly Follow the given JSON output format
+- Only run one step at a time.
+- The sequence of steps is START (where user gives an input), PLAN (That can be multiple times) and finally OUTPUT (which is going to the displayed to the user).
+
+Output JSON Format:
+{ "step": "START" | "PLAN" | "OUTPUT", "content": "string" }
+Example:
+START: Hey, Can you solve 2 + 3 * 5 / 10
+PLAN: { "step": "PLAN": "content": "Seems like user is interested in math problem.
+PLAN: { "step": "PLAN": "content": "looking at the problem, we should solve this using BODMAS
+method" }
+PLAN: { "step": "PLAN": "content": "Yes, The BODMAS is correct thing to be done here" }  
+PLAN: { "step": "PLAN": "content": "first we must multiply 3 * 5 which is 15" }
+PLAN: { "step": "PLAN": "content": "now we must divide 15 / 10 which is 1.5" }
+PLAN: { "step": "PLAN": "content": "now we must add 2 + 1.5 which is 3.5" }
+OUTPUT: { "step": "OUTPUT", "content": "The final answer is 3.5" }
+
+"""
+response = client.chat.completions.create(
+    model = 'gemini-2.5-flash',
+    response_format={"type": "json_object"},
+    messages = [
+        {"role": "system" , "content" : SYSTEM_PROMPT },
+        {"role": "user", "content": "write a program for first 10 prime numbers in python" },
+        {"role": "assistant", "content": "" }
+    ]
+)
+
+print('-'*30)
+print(response.choices[0].message.content)
+print('-'*30)
--- hello_world/few.py
+++ hello_world/few.py
@@ -12,11 +12,12 @@ client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
+SYSTEM_PROMPT = "Classify the sentiment of the following text as positive, negative, or neutral.Text: The product is terrible. Sentiment: Negative Text: Super helpful, worth it Sentiment: Positive"
 response = client.chat.completions.create(
     model = 'gemini-2.5-flash',
     messages = [
-        {"role": "system", "content": "You are a Mathematician. You help users in solving maths queries. You only response to maths related queries and nothing else. If the query is not related to maths then say sorry and ask for maths related query."},
-        {"role": "user", "content": "write me python script to solve the sum of first 10 natural numbers." }
+        {"role": "system" , "content" : SYSTEM_PROMPT },
+        {"role": "user", "content": "Text: i eat cake! Sentiment:" }
     ]
 )
 
